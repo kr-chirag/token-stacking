f1cde5f1e7693720ade819cfc85af09daf94e5f6
kr-chirag <chirag.p@knackroot.com>
2025-04-01 09:02:23 +0530

    Update RewardUtils.sol

--- contracts/utils/RewardUtils.sol
+++ contracts/utils/RewardUtils.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.28;
 
 library RewardUtils {
+
     struct Reward {
         uint256 amount; // amount to distribute for the entire duration
         uint256 durationToDistribute; // duration in which the amount should be distributed
@@ -17,47 +18,34 @@ library RewardUtils {
     }
 
     function _duration(
-        Reward storage reward,
-        uint256 _currentTime
+        Reward storage reward
     ) private view returns (uint256) {
-        if (_currentTime > reward.periodFinish) {
+        if (block.timestamp > reward.periodFinish) {
             if (reward.periodFinish < reward.lastUpdated) return 0;
             return reward.periodFinish - reward.lastUpdated;
         } else {
-            return _currentTime - reward.lastUpdated;
+            return block.timestamp - reward.lastUpdated;
         }
     }
 
     function _rewardPerTokenAccumulated(
         Reward storage reward,
-        uint256 _totalSupply,
-        uint256 _currentTime
-    ) private returns (uint256) {
-        uint256 recent = reward.rewardPerTokenAccumulated;
-        if (_totalSupply == 0) return recent;
-        uint256 current = (reward.rewardPerSecond *
-            _duration(reward, _currentTime)) / _totalSupply;
-        reward.rewardPerTokenAccumulated = recent + current;
-        return reward.rewardPerTokenAccumulated;
+        uint256 _totalSupply
+    ) private view returns (uint256) {
+        if (_totalSupply == 0) return 0;
+        return reward.rewardPerTokenAccumulated + (reward.rewardPerSecond *
+            _duration(reward) * 1e18 ) / _totalSupply;
     }
 
-    function calculateReward(
-        Reward storage reward,
-        address _account,
-        uint256 _stakedAmount,
-        uint256 _totalSupply
-    ) internal returns (uint256) {
-        uint256 _currentTime = block.timestamp;
-        uint256 currentRewardPerToken = _rewardPerTokenAccumulated(
-            reward,
-            _totalSupply,
-            _currentTime
-        );
-        uint256 applicableRewardPerToken = currentRewardPerToken -
-            reward.lastRewardPerTokenAccumulated[_account];
-        uint256 rewardedAmount = _stakedAmount * applicableRewardPerToken;
-        reward.lastRewardPerTokenAccumulated[_account] = currentRewardPerToken;
-        reward.lastUpdated = _currentTime;
-        return rewardedAmount;
+    function calculateReward(Reward storage reward, address _account, uint256 _stake, uint256 _totalSupply) internal view returns(uint256){
+        uint256 applicable = _rewardPerTokenAccumulated(reward, _totalSupply) - reward.lastRewardPerTokenAccumulated[_account];
+        return applicable * _stake / 1e18 ;
+        
     }
+
+    function updateRewardDetails(Reward storage reward, uint256 _totalSupply) internal {
+        reward.rewardPerTokenAccumulated = _rewardPerTokenAccumulated(reward, _totalSupply);
+        reward.lastUpdated = block.timestamp;
+    }
+
 }
